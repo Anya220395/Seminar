308f363cdf8e32cde59a41e046ac07acf25b6988
Anya220395 <anya220395@mail.ru>
2022-11-05 20:58:37 +0700

    add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Напишите программу, которая 
+1. выводит случайное трёхзначное число и 
+2. удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98*/
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число  => {number}");
+
+/*int lastDigit = number % 10;
+int firstDigit = number / 100;
+int foundDigit = firstDigit * 10 + lastDigit;
+
+Console.WriteLine($"Новое число  => {foundDigit}");*/
+
+int DeleteSecondDigit(int num)
+{
+    int lastDigit = num % 10;
+    int firstDigit = num / 100;
+    int foundDigit = firstDigit * 10 + lastDigit;
+    return foundDigit;
+}
+
+int result = DeleteSecondDigit (number);
+Console.WriteLine($"Новое число  => {result}");
+
+
