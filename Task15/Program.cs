1cc531e0d3f258cacb8faa62f6eaa7e619c926a6
Anya220395 <anya220395@mail.ru>
2022-11-09 00:20:23 +0700

    add change Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -9,15 +9,6 @@
 Console.Write("Введите число от 1 до 7: ");
 int day = Convert.ToInt32(Console.ReadLine());
 
-int Monday = 1;
-int Tuesday = 2;
-int Wednesday = 3;
-int Thursday = 4;
-int Friday = 5;
-int Saturday = 6;
-int Sunday = 7;
-
-
 if (day == 6 || day == 7)
 {
     Console.WriteLine(" Выходной ");
