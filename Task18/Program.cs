8f63baf440593aba3d0e791c1b1bfd801a5a915f
Anya220395 <anya220395@mail.ru>
2022-11-09 00:22:44 +0700

    add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая позаданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).*/
+
+Console.Write("Введите номер четверти: ");
+string quater = Console.ReadLine();
+
+string Quater(string quaterXY)
+{
+    if (quater == "1") return "x > 0 : y > 0";
+    if (quater == "2") return "x < 0 : y > 0";
+    if (quater == "3") return "x < 0 : y < 0";
+    if (quater == "4") return "x > 0 : y < 0";
+    return "Введен некорректный номер четверти";
+}
+string xy = Quater(quater);
+Console.WriteLine(xy);
