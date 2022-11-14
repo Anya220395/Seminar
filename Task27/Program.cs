2840eca07bf6044d6a7ce11242c1d65444b19137
Anya220395 <anya220395@mail.ru>
2022-11-14 20:11:51 +0700

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая 
+1.принимает на вход число и 
+2.выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int SumNumbers(int num)
+{
+    int sum = default;
+    int count = 0; // методом накопления записываем сумму чисел
+    while (num > 0) //пока число>0 отсекаем % последнее число и записываем его в сумму
+    {
+        sum = sum + num % 10;
+        num = num/10;
+        count++;
+    }
+    return sum;
+}
+
+int sumDigit = SumNumbers(number);
+Console.WriteLine(sumDigit);
